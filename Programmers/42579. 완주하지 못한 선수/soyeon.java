1827814f49c7a9091712fcbf655171de7d2217b9
soyeon Park <sypark9646@gmail.com>
2020-03-08 16:04:23 +0900

    1010점

---
+++ Programmers/42579. 완주하지 못한 선수/soyeon.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        //동명이인이 있을 수 있으므로 set은 사용 불가
+    	String answer = "";
+        HashMap<String, Integer> map=new HashMap<String, Integer>();
+        for(int i=0;i<participant.length;i++) {
+        	if(map.containsKey(participant[i])) {
+        		map.put(participant[i], map.get(participant[i])+1);
+        	}
+        	else {
+        		map.put(participant[i], 1);
+        	}
+        }
+        
+        for(int i=0;i<completion.length;i++) {
+        	if(map.get(completion[i])==1) {
+        		map.remove(completion[i]);
+        	}
+        	else {
+        		map.put(completion[i], map.get(completion[i])-1);
+        	}
+        }
+        Set<String> set= map.keySet();
+        Iterator it=set.iterator();
+        return it.next().toString();
+    }
+}
