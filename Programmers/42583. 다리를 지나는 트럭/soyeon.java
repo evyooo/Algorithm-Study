85e8f5e0c0ab04db998daafe6477d2cbfcacf3b9
soyeon Park <sypark9646@gmail.com>
2020-03-15 13:06:09 +0900

    Update soyeon.java

--- Programmers/42583. 다리를 지나는 트럭/soyeon.java
+++ Programmers/42583. 다리를 지나는 트럭/soyeon.java
@@ -4,30 +4,30 @@ class Solution {
         Queue<Integer> que=new LinkedList<Integer>();
 
         int sum=0;//트럭무게
-		int num=0;//트럭개수
-		int time=0;
-		
-		for(int i=0;i<weights.length;i++){
-			int train=weights[i];
-			while(true){
-				time++;
-				if(num==bridge_length){//트럭이 꽉 차 있으면
-					num--;
-					sum-=que.element();
-					que.remove();
-				}
-				if(sum+train>weight){//트럭이 무게를 못버티면
-					num++;
-					que.add(0);
-				}
-				else{
-					num++;
-					sum+=train;
-					que.add(train);
-					break;
-				}
+	int num=0;//트럭개수
+	int time=0;
+
+	for(int i=0;i<weights.length;i++){
+		int train=weights[i];
+		while(true){
+			time++;
+			if(num==bridge_length){//트럭이 꽉 차 있으면
+				num--;
+				sum-=que.element();
+				que.remove();
+			}
+			if(sum+train>weight){//트럭이 무게를 못버티면
+				num++;
+				que.add(0);
+			}
+			else{
+				num++;
+				sum+=train;
+				que.add(train);
+				break;
 			}
 		}
-		return time+bridge_length;
+	}
+	return time+bridge_length;
     }
 }
