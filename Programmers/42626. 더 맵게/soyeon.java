b89cb4296340dfb3cdb24a7cdaf3b60fb508b136
soyeon Park <sypark9646@gmail.com>
2020-03-20 21:10:59 +0900

    Update soyeon.java

--- Programmers/42626. 더 맵게/soyeon.java
+++ Programmers/42626. 더 맵게/soyeon.java
@@ -13,22 +13,23 @@ class Solution {
         return answer;
     }
     public boolean isFinished(PriorityQueue<Integer> scoville, int K) {
+	//음식을 섞는것이 끝났는지 확인
     	Iterator it=scoville.iterator();
     	while(it.hasNext()){
     		if((int)it.next()<K) {
     			return false;
     		}
     	}
-    	return true;
+    	return true; //모든 값이 K보다 크면 끝낸다
     }
     public int mixTwo(PriorityQueue<Integer> scoville, int K) {
     	int ans=0;
     	while(true) {
-    		if(scoville.size()==1 && scoville.peek()<K)
+    		if(scoville.size()==1 && scoville.peek()<K) //K이상을 만들 수 없는 경우
     			return -1;
     		
     		int sc1=scoville.poll();
-    		if(sc1>=K) break;
+    		if(sc1>=K) break; //가장 작은 값이 K이상인 경우 멈춘다
     		
     		int sc2=scoville.poll();
     		scoville.add(sc1+2*sc2);
