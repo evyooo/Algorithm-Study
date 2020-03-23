2eb46a404ebc0c41c30a49bf3728590795dd0f54
Yoojin Park <yooooojining@gmail.com>
2020-03-23 19:40:47 +0900

    Update yoojin.py

--- Programmers/42629. 라면공장/yoojin.py
+++ Programmers/42629. 라면공장/yoojin.py
@@ -5,9 +5,11 @@ def solution(stock, dates, supplies, k):
     index = 0
     h = []
     
+    # 하루에 1톤씩 사용하니까 stock이 k를 넘기기 전까지 계속
     while(stock < k):
         for i in range(index, len(dates)):
             if dates[i] <= stock:
+                # maxheap으로 사용하기위해 -를 달아줌
                 heapq.heappush(h, -supplies[i])
                 idx = i + 1
             else:
