1df34a213443ab22530fb276397222891fde89fa
astrobkk <61494135+astrobkk@users.noreply.github.com>
2020-03-30 18:45:13 +0900

    Update minji.java

--- 선택문제/1260. DFS와 BFS/minji.java
+++ 선택문제/1260. DFS와 BFS/minji.java
@@ -4,18 +4,18 @@ public class Main {
 	static ArrayList<Integer> a[];
 	static boolean c[];
 
-	public static void dfs(int x) {
+	public static void dfs(int x) { //dfs구현
 		if (c[x])
 			return;
 		c[x] = true;
 		System.out.print(x+" ");
-		for (int y : a[x]) {
+		for (int y : a[x]) { 
 			if (c[y] == false)
 				dfs(y);
 		}
 	}
 
-	public static void bfs(int start) {
+	public static void bfs(int start) { //bfs구현
 		Queue<Integer> q = new LinkedList();
 		q.add(start);
 		c[start]=true;
@@ -42,7 +42,7 @@ public class Main {
 		for (int i = 1; i <= n; i++)
 			a[i] = new ArrayList<Integer>();
 
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < m; i++) { //pair로 양방향 간선 u,v를 서로 이어준다.
 			int u = kbd.nextInt();
 			int v = kbd.nextInt();
 			a[u].add(v);
@@ -50,7 +50,7 @@ public class Main {
 		}
 		
 		for(int i=1;i<=n;i++)
-			Collections.sort(a[i]);
+			Collections.sort(a[i]); //정렬해준다.
 
 		c = new boolean[n + 1];
 		dfs(start);
